5cfee082573fb286858c0535546dc26781a59a7a
Justin Kirk <justin.c.kirk@gmail.com>
2026-02-19 20:55:03 -0500

    creates a canvas class

--- js/svg.js
+++ js/svg.js
@@ -1,51 +1,132 @@
-function draggable(canvas) {
-    let selected = null;
-    // let svg = event.target;
-    let svg = canvas;
-    let offset = { x: 0, y: 0 };
-    // let newPos = { x: 0, y: 0 };
-    let origin = { x: 0, y: 0 };
+// function draggable(canvas) {
+//     let selected = null;
+//     let svg = canvas;
+//     let offset = { x: 0, y: 0 };
+//     let origin = { x: 0, y: 0 };
 
-    svg.addEventListener("mousedown", startDrag);
-    svg.addEventListener("mousemove", drag);
-    svg.addEventListener("mouseup", endDrag);
-    svg.addEventListener("mouseleave", endDrag);
+//     svg.addEventListener("mousedown", startDrag);
+//     svg.addEventListener("mousemove", drag);
+//     svg.addEventListener("mouseup", endDrag);
+//     svg.addEventListener("mouseleave", endDrag);
 
-    function startDrag(event) {
-        event.preventDefault();
-        let target = event.target;
-        offset = getMouse(event);
+//     function startDrag(event) {
+//         event.preventDefault();
+//         let target = event.target;
+//         offset = getMouse(event);
 
-        if (target.classList.contains("draggable")) {
-            selected = target;
-            origin = {
-                x: selected.cx.baseVal.value,
-                y: selected.cy.baseVal.value,
-            };
-        }
+//         if (target.classList.contains("draggable")) {
+//             selected = target;
+//             origin = {
+//                 x: selected.cx.baseVal.value,
+//                 y: selected.cy.baseVal.value,
+//             };
+//         }
+//     }
+
+//     function drag(event) {
+//         event.preventDefault();
+//         if (selected) {
+//             let mousePos = getMouse(event);
+//             let dx = mousePos.x - offset.x;
+//             let dy = mousePos.y - offset.y;
+//             let newPos = { x: origin.x + dx, y: origin.y + dy };
+
+//             selected.setAttributeNS(null, "cx", newPos.x);
+//             selected.setAttributeNS(null, "cy", newPos.y);
+//         }
+//     }
+
+//     function endDrag(event) {
+//         event.preventDefault();
+//         selected = null;
+//     }
+
+//     function getMouse(event) {
+//         event.preventDefault();
+//         var CTM = svg.getScreenCTM();
+//         if (event.touches) {
+//             event = event.touches[0];
+//         }
+
+//         return {
+//             x: (event.clientX - CTM.e) / CTM.a,
+//             y: (event.clientY - CTM.f) / CTM.d,
+//         };
+//     }
+// }
+
+window.addEventListener("load", () => {
+    // const svgCanvas = document.getElementById("svg-canvas");
+
+    const circle = new Circle(200, 200, 100);
+    // const circle2 = new Circle(300, 200, 100);
+    // draggable(svgCanvas);
+    // svgCanvas.appendChild(circle.svg);
+    // svgCanvas.appendChild(circle2.svg);
+
+    const canvas = new Canvas("svg-canvas");
+    canvas.init();
+    canvas.draw(circle);
+});
+
+const SVG_NAMESPACE = "http://www.w3.org/2000/svg";
+
+class Canvas {
+    constructor(canvasId) {
+        this.canvas = document.getElementById(canvasId);
+
+        this.selected = null;
+        this.origin = { x: 0, y: 0 };
+        this.offset = { x: 0, y: 0 };
     }
 
-    function drag(event) {
-        event.preventDefault();
-        if (selected) {
-            let mousePos = getMouse(event);
-            let dx = mousePos.x - offset.x;
-            let dy = mousePos.y - offset.y;
-            let newPos = { x: origin.x + dx, y: origin.y + dy };
+    init() {
+        this.canvas.addEventListener("mousedown", (event) => {
+            event.preventDefault();
+            const target = event.target;
+            this.offset = this.getMouse(event);
 
-            selected.setAttributeNS(null, "cx", newPos.x);
-            selected.setAttributeNS(null, "cy", newPos.y);
-        }
+            if (target.classList.contains("draggable")) {
+                this.selected = target;
+                this.origin = {
+                    x: this.selected.cx.baseVal.value,
+                    y: this.selected.cy.baseVal.value,
+                };
+            }
+        });
+
+        this.canvas.addEventListener("mousemove", (event) => {
+            event.preventDefault();
+            if (this.selected) {
+                let mousePos = this.getMouse(event);
+                let dx = mousePos.x - this.offset.x;
+                let dy = mousePos.y - this.offset.y;
+                let newPos = { x: this.origin.x + dx, y: this.origin.y + dy };
+
+                this.selected.setAttributeNS(null, "cx", newPos.x);
+                this.selected.setAttributeNS(null, "cy", newPos.y);
+            }
+        });
+
+        this.canvas.addEventListener("mouseup", (event) => {
+            event.preventDefault();
+            this.selected = null;
+        });
+
+        this.canvas.addEventListener("mouseleave", (event) => {
+            event.preventDefault();
+            this.selected = null;
+        });
     }
 
-    function endDrag(event) {
-        event.preventDefault();
-        selected = null;
+    draw(shape) {
+        shape.draw();
+        this.canvas.appendChild(shape.svg);
     }
 
-    function getMouse(event) {
+    getMouse(event) {
         event.preventDefault();
-        var CTM = svg.getScreenCTM();
+        var CTM = this.canvas.getScreenCTM();
         if (event.touches) {
             event = event.touches[0];
         }
@@ -56,18 +137,6 @@ function draggable(canvas) {
         };
     }
 }
-
-window.addEventListener("load", () => {
-    const svgCanvas = document.getElementById("svg-canvas");
-    console.log(svgCanvas);
-    const circle = new Circle(200, 200, 100);
-    const circle2 = new Circle(300, 200, 100);
-    draggable(svgCanvas);
-    svgCanvas.appendChild(circle.svg);
-    svgCanvas.appendChild(circle2.svg);
-});
-
-const SVG_NAMESPACE = "http://www.w3.org/2000/svg";
 
 class Circle {
     constructor(x, y, r, canvas) {
@@ -100,101 +169,5 @@ class Circle {
         this.posY = y;
         this.circle.setAttribute("cx", parseInt(this.posX));
         this.circle.setAttribute("cy", parseInt(this.posY));
-    }
-}
-
-function makeDraggable(evt) {
-    var svg = evt.target;
-
-    svg.addEventListener("mousedown", startDrag);
-    svg.addEventListener("mousemove", drag);
-    svg.addEventListener("mouseup", endDrag);
-    svg.addEventListener("mouseleave", endDrag);
-    // svg.addEventListener("touchstart", startDrag);
-    // svg.addEventListener("touchmove", drag);
-    // svg.addEventListener("touchend", endDrag);
-    // svg.addEventListener("touchleave", endDrag);
-    // svg.addEventListener("touchcancel", endDrag);
-
-    var selectedElement,
-        offset,
-        transform,
-        bbox,
-        minX,
-        maxX,
-        minY,
-        maxY,
-        confined;
-
-    var boundaryX1 = 10.5;
-    var boundaryX2 = 30;
-    var boundaryY1 = 2.2;
-    var boundaryY2 = 19.2;
-
-    function startDrag(evt) {
-        if (evt.target.classList.contains("draggable")) {
-            selectedElement = evt.target;
-            offset = getMousePosition(evt);
-
-            // Make sure the first transform on the element is a translate transform
-            var transforms = selectedElement.transform.baseVal;
-
-            if (
-                transforms.length === 0 ||
-                transforms.getItem(0).type !==
-                    SVGTransform.SVG_TRANSFORM_TRANSLATE
-            ) {
-                // Create an transform that translates by (0, 0)
-                var translate = svg.createSVGTransform();
-                translate.setTranslate(0, 0);
-                selectedElement.transform.baseVal.insertItemBefore(
-                    translate,
-                    0
-                );
-            }
-
-            // Get initial translation
-            transform = transforms.getItem(0);
-            offset.x -= transform.matrix.e;
-            offset.y -= transform.matrix.f;
-
-            confined = evt.target.classList.contains("confine");
-            if (confined) {
-                bbox = selectedElement.getBBox();
-                minX = boundaryX1 - bbox.x;
-                maxX = boundaryX2 - bbox.x - bbox.width;
-                minY = boundaryY1 - bbox.y;
-                maxY = boundaryY2 - bbox.y - bbox.height;
-            }
-        }
-    }
-
-    function drag(evt) {
-        if (selectedElement) {
-            evt.preventDefault();
-
-            var coord = getMousePosition(evt);
-            var dx = coord.x - offset.x;
-            var dy = coord.y - offset.y;
-
-            if (confined) {
-                if (dx < minX) {
-                    dx = minX;
-                } else if (dx > maxX) {
-                    dx = maxX;
-                }
-                if (dy < minY) {
-                    dy = minY;
-                } else if (dy > maxY) {
-                    dy = maxY;
-                }
-            }
-
-            transform.setTranslate(dx, dy);
-        }
-    }
-
-    function endDrag(evt) {
-        selectedElement = false;
     }
 }
